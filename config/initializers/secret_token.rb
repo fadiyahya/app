5c134c7c6c64980caf8cb7109ee241fae61e0928
Fadi Yahya <fadiyahya@gmail.com>
2012-10-09 20:52:25 -0700

    Post scaffolding installed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App::Application.config.secret_token = 'ad1cdfa2a70898cd7c1aa169d712dddc35fd3ca3325b32ab3c513048026768fd766b4fb379508a0fd8a2608e29803448bbdf0d219d8266d1b7a0f810a304890e'
