e311cf7c52a5da50443a3bdcaa4f882738436a3a
Fadi Yahya <fadiyahya@gmail.com>
2012-10-10 00:12:16 -0700

    Josh help
    The lifespan form is list - The user views shows email in the main page

---
+++ app/models/post.rb
@@ -0,0 +1,12 @@
+class Post < ActiveRecord::Base
+
+ searchable do
+    text :title, :content
+    end
+
+  belongs_to :user
+  attr_accessible :content, :lifespan, :title
+  
+  validates :title, :content, :lifespan, :presence => true
+  validates :lifespan, :numericality => { :only_integer => true , :greater_than_or_equal_to => 5, :less_than_or_equal_to => 25}
+end
